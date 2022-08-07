91e37ba8d8a0d93f53d589b6d6556694e567826d
ukonulucky <ukonulucky@gmail.com>
2022-08-07 08:41:49 +0000

    cloudinary_crud

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import './App.css';
+import Gallary from "./component/gallary/Gallary";
+import Upload from "./component/upload/Upload";
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Routes>
+          <Route path="/" element={
+            <Upload />
+          } />
+           <Route path="/gallary" element={
+            <Gallary />
+          } />
+     </Routes>
+      </div>
+      </Router>
   );
 }
 
